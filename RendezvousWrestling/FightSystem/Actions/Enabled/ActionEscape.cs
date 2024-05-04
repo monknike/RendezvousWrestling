daef54a98c3441d264abaadc46c97e20d0ebaa1e
monknike <85686946+monknike@users.noreply.github.com>
2024-05-04 11:50:42 +0100

    Changed each move's buff to max 5 rather than 10

--- RendezvousWrestling/FightSystem/Actions/Enabled/ActionEscape.cs
+++ RendezvousWrestling/FightSystem/Actions/Enabled/ActionEscape.cs
@@ -59,7 +59,7 @@ namespace RendezvousWrestling.FightSystem.Actions.Enabled
 
         public override int AddBonusesToRollFromStats()
         {
-            return base.AddBonusesToRollFromStats() + (int)Math.Ceiling(this.Attacker.CurrentDexterity / 10m);
+            return base.AddBonusesToRollFromStats() + (int)Math.Ceiling(this.Attacker.CurrentDexterity / 20m);
         }
 
         public override void CheckRequirements()
--- RendezvousWrestling/FightSystem/Actions/Enabled/ActionHighRisk.cs
+++ RendezvousWrestling/FightSystem/Actions/Enabled/ActionHighRisk.cs
@@ -52,7 +52,7 @@ namespace RendezvousWrestling.FightSystem.Actions.Enabled
 
         public override int AddBonusesToRollFromStats()
         {
-            return (int)Math.Ceiling(this.Attacker.CurrentDexterity / 10m);
+            return (int)Math.Ceiling(this.Attacker.CurrentDexterity / 20m);
         }
 
         public override void OnMiss()
